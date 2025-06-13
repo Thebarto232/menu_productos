7f0e624a891d9ec4df3536c4c6a4d9fcb24dd764
johan <johanpinto232@gmail.com>
2025-06-13 12:08:43 -0500

    llamdaos petch

--- src/main.js
+++ src/main.js
@@ -1,24 +1,30 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// import './style.css'
+import './style.css';
+import * as categoria_controler from './views/productos/produc_categorias.js';
+import * as produc_categorias from './views/categoria/categoria_controler.js';
+const main_container = document.querySelector('.main_content');/**clase  */
+const listado = [
+  { name: 'productos', path: 'src/views/productos/index.html', src: produc_categorias },
+  { name: 'categoria ', path: 'src/views/categoria/index.html', src: categoria_controler }
+]
+const llamar = async (url) => {
+  const recibe = await fetch(`/src/views/${url}/index.html`);
+  return await recibe.text();
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+}
+window.addEventListener('hashchange', async () => {
 
-setupCounter(document.querySelector('#counter'))
+
+  const hast = location.hash.slice(1);
+
+  // if (window.location.hash === '#productos') {
+
+  //   main_container.innerHTML = await llamar('/productos');
+  // }
+  // if (window.location.hash === '#categorias') {
+  //   main_container.innerHTML = await llamar('/categoria');
+  // }
+  const encontrado = listado.find(({ name }) => name == hast);
+  main_container.innerHTML = await llamar(encontrado.name);
+});
+
